a96b771a614cdc4f69c5d5c30b9bfeb9ca0a8f7e
hamedmokri-ops <hamed.mokri@gmail.com>
2025-11-19 19:26:44 +0330

    Create ProfileScreen.tsx

---
+++ src/screens/ProfileScreen.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ProfileScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Profile Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#444",
+  },
+});
+
+export default ProfileScreen;
