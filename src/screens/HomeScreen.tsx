7b01e8410f347c53389366af7ccf95ff0d77058c
hamedmokri-ops <hamed.mokri@gmail.com>
2025-11-19 19:25:25 +0330

    Create HomeScreen.tsx

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Home Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#444",
+  },
+});
+
+export default HomeScreen;
